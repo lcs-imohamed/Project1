4c0babd91821c71792850a45e9ccbec9d260f326
Ilana Mohamed <Imohamed@lcs.on.ca>
2020-04-16 11:11:22 -0400

    Finished logic to move player when touched on screen

--- DiveIntoSpriteKit/GameScene.swift
+++ DiveIntoSpriteKit/GameScene.swift
@@ -8,8 +8,40 @@
 
 import SpriteKit
 
+class Person {
+    
+    // MARK: Properties
+    var hairColor : String
+    var age: Int
+    
+    // MARK: Initializers
+    init(hairColor: String, age: Int) {
+        self.hairColor = hairColor
+        self.age = age
+    }
+    
+    // MARK: Methods
+    
+    // Purpose: Increase the age by one
+    func birthday() {
+        age += 1
+    }
+
+}
+
+
+
 @objcMembers
 class GameScene: SKScene {
+    
+    // MARK: Properties
+    var touchingPlayer = false
+    let player = SKSpriteNode(imageNamed: "player-rocket.png")
+    
+    // MARK: Initializers
+    
+    
+    // MARK: Methods
     override func didMove(to view: SKView) {
         // this method is called when your game scene is ready to run
         //Add and position space background in scene 
@@ -24,8 +56,10 @@ class GameScene: SKScene {
             addChild(particles)
             
         //Add player to scene
-            let player = SKSpriteNode(imageNamed: "player-rocket.png")
+            
+            
             player.position.x = -400
+            player.zPosition = 1
             addChild(player)
             
         }
@@ -34,14 +68,42 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user touches the screen
+        //Find the first touch on the screen
+        guard let touch = touches.first else {
+            return }
+        
+        //Find the location of touch in game scene
+        let location = touch.location(in: self)
+        
+        //Find the list of nodes in location
+        let tappedNodes = nodes(at: location)
+        
+        //If the player is tapped set touching player to true
+        if tappedNodes.contains(player) {
+            touchingPlayer = true
+        }
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //If touchingPlayer is false then return
+        guard touchingPlayer else { return }
+        guard let touch = touches.first else { return }
+        
+        //If the player's touch is read correctly find touch's location in scene and set player position to that location
+        let location = touch.location(in: self)
+        player.position = location
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user stops touching the screen
+        touchingPlayer = false
     }
 
     override func update(_ currentTime: TimeInterval) {
         // this method is called before each frame is rendered
     }
+    
+    
 }
 
